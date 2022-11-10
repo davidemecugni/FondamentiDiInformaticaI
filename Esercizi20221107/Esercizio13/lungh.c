5ddeee74808c69a3537104e3e8844dfdf5e2a7d6
davidemecugni <299046@studenti.unimore.it>
2022-11-10 21:24:27 +0100

    Finito 13

---
+++ Esercizi20221107/Esercizio13/lungh.c
@@ -0,0 +1,9 @@
+#include "lungh.h"
+#include <stdint.h>
+size_t lungh(const char* str) {
+	size_t i = 0;
+	while (str[i] != '\0') {
+		i++;
+	}
+	return i;
+}
---
+++ Esercizi20221107/Esercizio13/lungh.h
@@ -0,0 +1,4 @@
+#if !defined LUNGH_H
+#define LUNGH_H
+extern size_t lungh(const char* str);
+#endif /*LUNGH_H*/
